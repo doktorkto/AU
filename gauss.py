bf91287c09df0953d5e0f18a4786b8f56c996624
doktorkto <32988757+doktorkto@users.noreply.github.com>
2020-02-29 22:08:20 +0300

    Update gauss.py

--- gauss.py
+++ gauss.py
@@ -1,25 +1,20 @@
-import sys
-import numpy as np 
+import numpy as np
 
-#a - matrix nxn of coefficients, b - rigth side in every par in this system, n - size of leg this matrix
-def gauss_method(a: np.array, b: np.array, n):  
-    for i in range(0, n - 1):
-        for j in range(0, n - 1):
-            a[i, j] /= a[i, i]
-        b[i] = b[i] / a[i, i]
-        for j in range(i + 1,n - 1):
-            for k in range(i, n - 1):
-                a[j, k] -= a[i, k] * a[j, i]
-            b[j] -= b[i] * a[j, i]
-    for i in range(0, n - 1):
-        for j in range(0, n - 2 - i):
-            a[n - 2 - i - j, n - 1 - i] -= a[n - 1 - i, n - 1 - i] * a[n - 2 - i - j, n - 1 - i]
-            b[n - 2 - i - j] -= b[n - 1 - i] * a[n - 1 - i, n - 1 - i]
+def gauss_method(a: np.array, b: np.array, n):
+    for i in range(0, n):
+        a[i] /= a[i, i]
+        b[i] /= a[i, i]
+   
+        for j in range(0, n):
+            if j != i:
+                a[j] -= a[i] * a[j, i]
+                b[j] -= b[i] * a[j, i]
+        for j in range(0, n):
+            print(a[j])
+        print('  ')
     return b
 
-s = np.array([[0.0, 2.0, 1.0], [1.0, 0.0 , 1.0], [0.0, 1.0, 1.0]], float)
-k = np.array([4.0, 2.0, 1.0], float)
-l = np.zeros(3)
+s = np.array([[1.0, 0.0 , 1.0], [0.0, 1.0, -1.0], [1.0, 1.0, 1.0]], float)
+k = np.array([4.0, 3.0, 5.0], float)
 l = gauss_method(s, k, 3)
-for i in range(0, 2):
-    print(l[i])
+print(l)
