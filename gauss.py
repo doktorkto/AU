aa30a287dd1997a198eb7f521f65ce3ed74e7f95
doktorkto <32988757+doktorkto@users.noreply.github.com>
2020-03-04 21:19:43 +0300

    Update gauss.py

--- gauss.py
+++ gauss.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numba import njit
+from numba import njit, prange
 
 s = np.array([[2.0, 1.0 , 1.0], [4.0, 1.0, 1.0], [2.0, 1.0, -1.0]], float)
 k = np.array([2.0, 6.0, 4.0], float)
@@ -15,6 +15,19 @@ def gauss_method(a: np.array, b: np.array, n):
                 b[j] -= b[i] * a[j, i]
                 a[j] -= a[i] * a[j, i]
     return b
+
+def gauss_method2(a: np.array, b: np.array, n):
+    for i in range(0, n):
+        b[i] /= a[i, i]
+        a[i] /= a[i, i]
+   
+        for j in range(0, n):
+            if j != i:
+                b[j] -= b[i] * a[j, i]
+                a[j] -= a[i] * a[j, i]
+    return b
+gauss_method2(s, k, 3)
+%timeit gauss_method2(s, k, 3)
 
 gauss_method(s, k, 3)
 %timeit gauss_method(s, k, 3)
